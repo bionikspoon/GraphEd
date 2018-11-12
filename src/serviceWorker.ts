b741c663c848f8158fcf80547847de74c935ad46
Manu Phatak <manu@thinkcerca.com>
2018-11-12 15:34:10 -0600

    fix cognitive complexity

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -63,38 +63,47 @@ function tryRegisterValidSW(swUrl: SWUrl, config?: IConfig) {
   }
 }
 
+function handlestateChange(
+  this: ServiceWorker,
+  registration: ServiceWorkerRegistration,
+  config: IConfig
+) {
+  if (this.state !== 'installed') return
+
+  if (navigator.serviceWorker.controller) {
+    // At this point, the updated precached content has been fetched,
+    // but the previous service worker will still serve the older
+    // content until all client tabs are closed.
+    console.log(
+      'New content is available and will be used when all tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+    )
+
+    // Execute callback
+    if (config.onUpdate) config.onUpdate(registration)
+  } else {
+    // At this point, everything has been precached.
+    // It's the perfect time to display a
+    // "Content is cached for offline use." message.
+    console.log('Content is cached for offline use.')
+
+    // Execute callback
+    if (config.onSuccess) config.onSuccess(registration)
+  }
+}
+
 async function registerValidSW(swUrl: SWUrl, config?: IConfig) {
   const registration = await navigator.serviceWorker.register(swUrl)
 
   registration.onupdatefound = () => {
     const installingWorker = registration.installing
     if (!installingWorker) return
+    if (!config) return
 
-    installingWorker.onstatechange = () => {
-      if (installingWorker.state !== 'installed') return
-      if (!config) return
-
-      if (navigator.serviceWorker.controller) {
-        // At this point, the updated precached content has been fetched,
-        // but the previous service worker will still serve the older
-        // content until all client tabs are closed.
-        console.log(
-          'New content is available and will be used when all ' +
-            'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
-        )
-
-        // Execute callback
-        if (config.onUpdate) config.onUpdate(registration)
-      } else {
-        // At this point, everything has been precached.
-        // It's the perfect time to display a
-        // "Content is cached for offline use." message.
-        console.log('Content is cached for offline use.')
-
-        // Execute callback
-        if (config.onSuccess) config.onSuccess(registration)
-      }
-    }
+    installingWorker.onstatechange = handlestateChange.bind(
+      installingWorker,
+      registration,
+      config
+    )
   }
 }
 
